20c7f339afe405dfa7582c3d1d528f079d6d979a
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-10-31 11:49:09 -0300

    chore: tailwind and packages updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
 import { Text, View } from 'react-native';
-import './global.css';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+			<Text>Testando se finalmente deu certo.</Text>
+		</View>
+	);
 }
