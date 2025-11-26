5ba833e1e0ba6cc9b95027239d776eebd1d62cbb
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-11-26 14:25:07 -0300

    fix: removed splash screen artificial delay

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,46 +1,16 @@
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
 
 import "../global.css";
 
-// Set the animation options. This is optional.
+// Set the animation options for the splash screen
 SplashScreen.setOptions({
 	duration: 1000,
 	fade: true,
 });
 
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-	const [isReady, setIsReady] = useState(false);
-
-	useEffect(() => {
-		async function doAsyncStuff() {
-			try {
-				// 3 seconds delay to simulate an async task
-				await new Promise((resolve) => setTimeout(resolve, 3000));
-			} catch (e) {
-				console.warn(e);
-			} finally {
-				setIsReady(true);
-			}
-		}
-
-		doAsyncStuff();
-	}, []);
-
-	useEffect(() => {
-		if (isReady) {
-			SplashScreen.hide();
-		}
-	}, [isReady]);
-
-	if (!isReady) {
-		return null;
-	}
-
 	return (
 		<>
 			<Stack
