05fd572affdd4bc6276e83eb19607d7906f35e55
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-11-27 17:58:57 -0300

    fix: removed camera for now

--- app/submit/step1.tsx
+++ app/submit/step1.tsx
@@ -1,11 +1,10 @@
-import { Camera } from "@/components/camera";
 import { Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function SubmitFormStep1() {
 	return (
 		<View className="flex items-start justify-start gap-4 p-5">
-			<Camera />
+			{/* <Camera /> */}
 			<Link href="/submit/step2" asChild>
 				<TouchableOpacity className="bg-primary-400 w-full rounded px-4 py-2">
 					<Text className="text-white">Câmera</Text>
